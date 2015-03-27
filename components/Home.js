9fa6a8cd51cc2b585d250179511f79c6feb9ffe9
Eric Clemmons <eric@smarterspam.com>
2015-03-27 18:46:27 -0500

    setState example

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 class Home extends React.Component {
+  constructor() {
+    this.state = {
+      color: "black"
+    };
+  }
+
+  changeColor() {
+    const newColor = Math.floor(Math.random() * 0xffffff).toString(16);
+
+    this.setState({
+      color: `#${newColor}`
+    });
+  }
+
   render() {
+    const styles = { color: this.state.color };
+
     return (
-      <h1>Howdy from <code>{this.props.path}</code>!</h1>
+      <h1 onMouseMove={this.changeColor.bind(this)} style={styles}>
+        Howdy from <code>{this.props.path}</code>!
+      </h1>
     );
   }
 }
